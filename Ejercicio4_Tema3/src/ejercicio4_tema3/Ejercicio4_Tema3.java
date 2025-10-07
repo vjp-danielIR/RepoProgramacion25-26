4057cf14aa9d843c1a5f6dd488766bbc148ab132
vjp-danielIR <diglesiasr04@informatica.iesvalledeljerteplasencia.es>
2025-10-07 15:44:22 +0200

    EJERCICIOS

--- Ejercicio4_Tema3/src/ejercicio4_tema3/Ejercicio4_Tema3.java
+++ Ejercicio4_Tema3/src/ejercicio4_tema3/Ejercicio4_Tema3.java
@@ -2,36 +2,35 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package ejercicio3_tema3;
- import java.util.Scanner;
+package ejercicio4_tema3;
+import java.util.Scanner;
 /**
  *
  * @author alumno
  */
-public class Ejercicio3_Tema3 {
+public class Ejercicio4_Tema3 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-     Scanner entrada = new Scanner(System.in);
-        //pedir primer numero
-        System.out.print("introduzca el primer número: ");
+       Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Por favor, introduzca el primer número: ");
         int num1 = entrada.nextInt();
-        //el segundo
-        System.out.print("introduzca un segundo número: ");
+
+        System.out.print("Ahora, introduzca un segundo número: ");
         int num2 = entrada.nextInt();
-            //el tercero
-        System.out.print("introduzca un tercer número: ");
+
+        System.out.print("Por último, introduzca un tercer número: ");
         int num3 = entrada.nextInt();
-            
-        //solo necesito un  Scanner entrada por que todos los valores son del mismo tipo
+
         int mayor;
 
         // Determinar el mayor de los tres números
-        if (num1 >= num2 && num1 >= num3) {
+        if (num1 <= num2 && num1 <= num3) {
             mayor = num1;
-        } else if (num2 >= num1 && num2 >= num3) {
+        } else if (num2 <= num1 && num2 <= num3) {
             mayor = num2;
         } else {
             mayor = num3;
@@ -39,5 +38,4 @@ public class Ejercicio3_Tema3 {
 
         System.out.println("El número mayor de los introducidos es el " + mayor);
     }
-    
 }
