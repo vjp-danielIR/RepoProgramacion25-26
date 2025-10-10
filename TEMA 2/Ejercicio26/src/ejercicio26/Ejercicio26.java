d15e4b065fbdd99d3c25b74902255ed16d563254
vjp-danielIR <diglesiasr04@informatica.iesvalledeljerteplasencia.es>
2025-10-10 20:02:10 +0200

    ARREGLO

--- TEMA 2/Ejercicio26/src/ejercicio26/Ejercicio26.java
+++ TEMA 2/Ejercicio26/src/ejercicio26/Ejercicio26.java
@@ -8,8 +8,8 @@ package ejercicio26;
 import java.util.Scanner;
         
 /**
- *
- * @author alumno
+ *MOSTRAR NUMEROS POR SEPARADO
+ * @author Daniel Iglesias
  */
 public class Ejercicio26 {
 
@@ -19,13 +19,14 @@ public class Ejercicio26 {
     public static void main(String[] args) {
         Scanner numeros = new Scanner (System.in);
         System.out.println(" Dime  4 nuemros: " );
-        
+        //VOY DIVIDIENDO Y SACANDO CADA NUMERO 
           int num1 = numeros.nextInt();
-          int num2 = numeros.nextInt();
-          int num3 = numeros.nextInt();
-          int num4 = numeros.nextInt();
+          int num0 = num1 / 1000; 
+          int num2 = (num1 %1000) /100;
+          int num3 = ((num1%1000)%100) / 10;
+          int num4 = (((num1%1000)%100)%10);
           
-          System.out.println(" El primer numero es : " + num1 + " el segundo:" + num2 + " el tercero es: " + num3 + " el cuerto es: " + num4);
+          System.out.println(" El primer numero es : " + num0 + " el segundo:" + num2 + " el tercero es: " + num3 + " el cuerto es: " + num4);
     }
     
 }
