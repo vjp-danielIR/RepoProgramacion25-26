d8f8a0b2133bded47af55541337baabd92693cd4
1DAW - Daniel Iglesias Ramos <diglesiasr04@informatica.iesvalledeljerteplasencia.es>
2025-10-12 14:59:06 +0200

    Arreglo

--- Tema3/Ejercicio6_Tema3/src/ejercicio6_tema3/Ejercicio6_Tema3.java
+++ Tema3/Ejercicio6_Tema3/src/ejercicio6_tema3/Ejercicio6_Tema3.java
@@ -16,17 +16,24 @@ public class Ejercicio6_Tema3 {
     public static void main(String[] args) {
             Scanner entrada = new Scanner (System.in);
             System.out.println("dime la nota que sacastes= ");
+            // Leemos la nota con decimales
             float nota = entrada.nextFloat();
-            
+
+            // Validamos y clasificamos la nota con una estructura if-else encadenada
             if (nota >= 0 && nota <= 4){
+                // Si la nota está entre 0 y 4  Suspensa
                 System.out.println(" tu nota = " + nota + " es Suspensa ");
             } else if (nota <= 6){
+                // Si la nota es mayor que 4 y hasta 6  Aprobado 
                 System.out.println(" Tu nota =" + nota + " esta bien");
             } else if (nota <= 8){
+                // Si la nota es mayor que 6 y hasta 8  Notable
                 System.out.println(" tu nota = " + nota + " es un Notable");
             } else if (nota  <= 10){
+                // Si la nota es mayor que 8 y hasta 10  Sobresaliente
                 System.out.println("tu nota = " + nota + "es un SOBRESALIENTER");
             } else {
+                 // Cualquier otra entrada fuera del rango 0-10 se considera inválida
                  System.out.println("la nota debe estar entre 0 y 10.");
         }
                 
