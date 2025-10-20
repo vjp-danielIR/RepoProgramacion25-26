bf7948066749b2a7de77e07a9f30a4f5887b05dd
vjp-danielIR <diglesiasr04@informatica.iesvalledeljerteplasencia.es>
2025-10-20 17:15:31 +0200

    ultimo arreglo

--- Tema3/Ejercicio16_Tema3/src/ejercicio16_tema3/Ejercicio16_Tema3.java
+++ Tema3/Ejercicio16_Tema3/src/ejercicio16_tema3/Ejercicio16_Tema3.java
@@ -14,24 +14,20 @@ public class Ejercicio16_Tema3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         // Contador de números impares
-        int contador = 0; 
-        
-        System.out.print("Los números impares existentes entre el número 20 y el 160 son: ");
-        
+  
+        // Contador de números impares
+        int contador = 0;
+
+        // Imprimir la frase solo una vez
+        System.out.println("Los números impares existentes entre el número 20 y el 160 son:");
+
         // Bucle desde 21 hasta 159 (los impares en ese rango)
         for (int i = 21; i < 160; i += 2) {
             System.out.println(i);
-            
-            // Comprobar si i es mayor que 160
-            if (i < 160) {
-                System.out.print("Los números impares existentes entre el número 20 y el 160 son: ");
-            }
-            
-            //Contamos los números impares
-            contador++; 
+            // Contamos los números impares
+            contador++;
         }
         // Salto de línea y cantidad total
-        System.out.println("La cantidad de números impares impresos han sido: " + contador);
+        System.out.println("La cantidad de números impares impresos ha sido: " + contador);
     }
 }
