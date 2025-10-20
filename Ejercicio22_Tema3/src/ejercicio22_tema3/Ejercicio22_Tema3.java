9f4df5e889bc51fd672a7abe667c9a07caa9d5cb
vjp-danielIR <diglesiasr04@informatica.iesvalledeljerteplasencia.es>
2025-10-20 18:05:07 +0200

    Ejercicios

--- Ejercicio22_Tema3/src/ejercicio22_tema3/Ejercicio22_Tema3.java
+++ Ejercicio22_Tema3/src/ejercicio22_tema3/Ejercicio22_Tema3.java
@@ -2,20 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package ejercicio21_tema3;
+package ejercicio22_tema3;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 /**
- * dividr dos numeros y que no pueda dividir entre 0
+ * lo mismo que el 21 solo que sumando y que le salte error si pone una letra
  * @author Daniel Iglesias
  */
-public class Ejercicio21_Tema3 {
+public class Ejercicio22_Tema3 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //hacemos el scanner para pedir por pantalla y hacemos 2 variables para guardar los numeros
+      //hacemos el scanner para pedir por pantalla y hacemos 2 variables para guardar los numeros
         Scanner entrada = new Scanner (System.in);
          int  num1;
         int num2;
@@ -27,8 +27,8 @@ public class Ejercicio21_Tema3 {
             System.out.print("Introduce el segundo número: ");
             num2 = entrada.nextInt();
             
-           System.out.println("el resultado de dividir " + num1 + " entre " + num2 + " es igual a = " + (num1 / num2));
+           System.out.println("el resultado de dividir " + num1 + " + " + num2 + " es igual a = " + (num1 + num2));
        //en caso de que el usuario quiera dividir entre 0 le saldrea este esrror 
-    } catch(ArithmeticException e) {
-            System.out.println("no puedes dividir entre 0");
+    } catch(java.util.InputMismatchException e) {
+            System.out.println("no puedes sumar letras solo numeros");
 }}}
