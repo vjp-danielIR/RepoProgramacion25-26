5ee46a8b5417bf129f87690a220053bac6e8854c
vjp-danielIR <diglesiasr04@informatica.iesvalledeljerteplasencia.es>
2025-10-21 18:05:24 +0200

    TEMA4

---
+++ TEMA_4/METODOS_ESTATICOS_T4/src/Utilidades_TEMA4/UTILIDADES.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.UTILIDADES to edit this template
+ */
+package Utilidades_TEMA4;
+
+/**
+ *
+ * @author alumno
+ */
+public class UTILIDADES {
+    
+}
---
+++ TEMA_4/METODOS_ESTATICOS_T4/src/metodos_estaticos_t4/CALCULADORA.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio27_tema3;
+
+/**
+ *metodo que devuelve la suma de dos numeros 
+ * pasados por parametros 
+ * 
+ * @author alumno
+ */
+public class Calculadora {
+
+    /**
+     * Método que devuelve la suma de dos números pasados por parámetros.
+     * 
+     * @param num1 
+     * @param num2 
+     * @return la suma de num1 y num2
+     * @author alumno
+     */
+    public static int suma(int num1, int num2){
+        return num1 + num2;
+    }
+
+    /**
+     * Método que devuelve la resta de dos números pasados por parámetros.
+     * 
+     * @param num1 primer número
+     * @param num2 segundo número
+     * @return la resta de num1 menos num2
+     * @author alumno
+     */
+    public static int resta(int num1, int num2){
+        return num1 - num2;
+    }
+}
