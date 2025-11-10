0ee5427280fbd498afef9c87482a1ae453b7e1af
vjp-danielIR <diglesiasr04@informatica.iesvalledeljerteplasencia.es>
2025-11-10 18:05:20 +0100

    empezando ejercicio 16

---
+++ TEMA_5/T5_Ejercicio16/src/t5_ejercicio16/Alumno.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package t5_ejercicio16;
+
+/**
+ *
+ * @author alumno
+ */
+public class Alumno {
+    private String nombre;
+    private int nota;
+    
+    
+    public Alumno(){
+    this.nombre="";
+    nota =0;
+    }
+    
+    public Alumno(String nombre, int nota){
+    nombre=nombre;
+    nota=nota;
+    }
+    
+    
+}
+
