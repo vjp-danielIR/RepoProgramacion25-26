d8faa5fc5f6c74aaca184ba0c427a8d8e52e57be
vjp-danielIR <diglesiasr04@informatica.iesvalledeljerteplasencia.es>
2025-11-14 19:00:19 +0100

    corregir cola correos

--- TEMA_5/Cola_Correos_/src/cola_correos_/Cola_Correos_.java
+++ TEMA_5/Cola_Correos_/src/cola_correos_/Cola_Correos_.java
@@ -7,17 +7,14 @@
  */
 package cola_correos_;
 import java.util.Scanner;
-
+import java.util.InputMismatchException;
 public class Cola_Correos_ {
 
-    /**
-     * @param args the command line arguments
-     */
     //definimos las constantes aun que no se si estan bien segun yo habia que poner un static al lado del final pero me da error de compilacion
-    final char INICIOMESA= 'A';
-    final char FINALMESA = 'F';
-    final int NUMEROENVIO = 1;
-    final int NUMERORECOGIDA = 1;
+    public final static char INICIOMESA= 'A';
+    public final static char FINALMESA = 'F';
+    public static int numEnvio = 1;
+    public  static int numRecogida = 1;
     public static void main(String[] args) {
     //llamamos al metodo menu para mostarlo
      mostrarMenu();
@@ -34,13 +31,16 @@ public class Cola_Correos_ {
     
     //este metodo pide la opcion al usuario y segun lo que eliga hara una cosa
     public static int pedirOpcion(){
-        
      Scanner entrada=new Scanner (System.in);
-        System.out.println("dime una opcion");
-      int opcion = entrada.nextInt();
+     int opcion= 0;
+         System.out.println("dime una opcion");
+   
+         opcion = entrada.nextInt();}  
+   
+        
             switch (opcion) {
                 case 1://si escoge el 1 es que quiere enviar un paquete o carta y se le entrega un numero de espera
-                    generarNumeroEsperaEnvio();
+                    //generarNumeroEsperaEnvio();
                     break;
                 case 2://si escoge el dos le daremos un numero de recogida 
                     generarNumeroEsperaRecogida();
@@ -50,44 +50,48 @@ public class Cola_Correos_ {
                     break;
                 default: 
                     System.out.println("Opción no valida");
-                    }
-                return opcion;
-                }
+           return opcion;
+                }}
+   
+      
              //este metodo calculara una letra aleatoria entre la a y la f 
-    public static char generaraLetraAleatoria(){
+    public static char generaraLetraAleatoria(char INICIOMESA, char FINALMESA){
     //generar letra
-         final char INICIOMESA= 'A';
-    final char FINALMESA = 'F';
+    
     char letraAleatoria = (char) (Math.random() * (FINALMESA - INICIOMESA + 1) + INICIOMESA);
     return letraAleatoria;
     }
 
     // este metodo sumara 1 para que no haya conflictos por tener el mismo numero
-    public static int  incrementarNumeroEnvio(){
-    int NUMEROENVIO = 0;
-    NUMEROENVIO++;  
-    return NUMEROENVIO;
+    public static void  incrementarNumeroEnvio(){
+    numEnvio++;  
+    
     }
     //este metodo es lo mismo que el anterior solo que en recogida sumara 1 para que no haya conflictos de que 2 personas tengan el mismo numero
-    public static int incrementarNumeroRecogida(){
-    int NUMERORECOGIDA= 0;
-    NUMERORECOGIDA++;
-    return NUMERORECOGIDA;
+    public static void incrementarNumeroRecogida(){
+    numRecogida++;
+  
        }
     //en caso de que el usuairo escogiera el 1 lo trae aqui donde este metodo le dara un numero de espera y una mesa donde ir 
-    public static int generarNumeroEsperaEnvio(){
-    int NUMEROENVIO;
-         NUMEROENVIO = incrementarNumeroEnvio();
-    System.out.println("Su numero de envío es: E-" + NUMEROENVIO);
-        System.out.println("Vaya a la mesa: " + generaraLetraAleatoria());
-       return  NUMEROENVIO; //dev   uelve para que sume 1 en el metodo de numero envio para que no haya conflictos 
+    public static void generarNumeroEsperaEnvio(){
+        System.out.println("\nNumeo: E-" + numEnvio);
+        incrementarNumeroEnvio();//dev   uelve para que sume 1 en el metodo de numero envio para que no haya conflictos 
             }
                 //este metodo es para las recogidas de cartas o paquetes
-        public static int generarNumeroEsperaRecogida(){
-            int NUMERORECOGIDA;
-         NUMERORECOGIDA = incrementarNumeroRecogida();
-            
-         System.out.println("Su número de recogida es: R-" + NUMERORECOGIDA);
-        System.out.println("Diríjase a la mesa: " + generaraLetraAleatoria());
-       return NUMERORECOGIDA;
+        public static void generarNumeroEsperaRecogida(){
+            System.out.println("\nNumero R-" + numRecogida);
+            incrementarNumeroRecogida();
+}}
+public static void main(String[] args){
+int opcion= 0;
+do{
+try{
+mostrarMenu();
+opcion=pedirOpcion();
+    System.out.println("");
+}
+catch(InputMismatchException e){
+    System.out.println("opcion incorrecta");
+}
+while(opcion !=3);
 }}
