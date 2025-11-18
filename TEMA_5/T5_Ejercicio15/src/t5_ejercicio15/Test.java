0ed2520dd61ba8bc7461d5649529ccd4db3e1d4f
vjp-danielIR <diglesiasr04@informatica.iesvalledeljerteplasencia.es>
2025-11-18 18:04:32 +0100

    T5_Ejercicio15
    Direccion empleados

---
+++ TEMA_5/T5_Ejercicio15/src/t5_ejercicio15/Test.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package t5_ejercicio15;
+
+/**
+ *
+ * @author alumno
+ */
+public class Test {
+    
+}
--- TEMA_5/T5_Ejercicio23/src/t5_ejercicio23/T5_Ejercicio23.java
+++ TEMA_5/T5_Ejercicio23/src/t5_ejercicio23/T5_Ejercicio23.java
@@ -13,8 +13,6 @@ public class T5_Ejercicio23 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    
     
 }
