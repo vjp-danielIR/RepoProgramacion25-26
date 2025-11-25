baf25307f57efc38de5e6583ed9349a17d963ca3
vjp-danielIR <diglesiasr04@informatica.iesvalledeljerteplasencia.es>
2025-11-25 17:27:20 +0100

    T6_Ejercicio4

---
+++ TEMA_6/T6_Ejercicio4/src/t6_ejercicio4/Canidos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package t6_ejercicio4;
+
+/**
+ *
+ * @author alumno
+ */
+public abstract class Canidos extends Animales{
+    //constructores
+    public Canidos(){
+    super();
+    }
+    
+    public Canidos(String Nombre, int Edad, double Peso){
+    super(Nombre, Edad, Peso);
+    }
+}
