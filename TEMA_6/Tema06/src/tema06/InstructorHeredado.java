499ed8d289bfcf4d38f9aa87cae3e0d2b9a40618
vjp-danielIR <diglesiasr04@informatica.iesvalledeljerteplasencia.es>
2025-11-25 17:39:33 +0100

    Tema 6 extensión de objetos

---
+++ TEMA_6/Tema06/src/tema06/InstructorHeredado.java
@@ -0,0 +1,21 @@
+package tema06;
+
+/**
+ * Ninguna clase puede heredar de una clase final.
+ * @author d3stroya
+ */
+public class InstructorHeredado extends Instructor {
+    // ATRIBUTOS
+
+
+    // CONSTRUCTORES
+
+
+    // GETTERS Y SETTERS
+
+
+    // MÉTODOS
+
+
+    // TO STRING
+}
