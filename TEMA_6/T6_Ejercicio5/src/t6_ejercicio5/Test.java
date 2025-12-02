2dcc37603bf319f1ef2e280a96247bd101055111
vjp-danielIR <diglesiasr04@informatica.iesvalledeljerteplasencia.es>
2025-12-02 17:19:47 +0100

    Ejercicios terminados

---
+++ TEMA_6/T6_Ejercicio5/src/t6_ejercicio5/Test.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t6_ejercicio5;
+
+/**
+ *
+ * @author alumno
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("De forma comun ");
+       Ornitorrinco ornitorrinco1 = new Ornitorrinco(10, 12, "marron", "macho");
+       Murcielago murcielago1= new Murcielago(20, 3, "negro", "hembra");
+       
+        System.out.println("ORNITORRINCO ");
+       ornitorrinco1.getHuevos();
+       ornitorrinco1.incubarHuevos();
+       ornitorrinco1.mostrarNumHuevos();
+       ornitorrinco1.obtenerMensajeOviparo();
+       ornitorrinco1.ponerHuevos();
+        System.out.println("");
+        System.out.println("MURCIELAGO");
+        murcielago1.bajarAlturaVuelo();
+        murcielago1.aumentarAlturaVuelo();
+        murcielago1.getMertrosAltura();
+        murcielago1.obtenerMensajeVolador();
+      
+        System.out.println("");
+        System.out.println("== POLIMORFISMO ==");
+        System.out.println("");
+        
+        Mamifero orni= new Ornitorrinco(20, 24, "violeta", "hembra");
+        Mamifero murcia= new Murcielago(15, 7, "azul","hombre");
+        
+        System.out.println(orni);
+        System.out.println(murcia);
+    
+    
+    
+    }
+    
+}
---
+++ TEMA_6/T6_Ejercicio5/src/t6_ejercicio5/Volador.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package t6_ejercicio5;
+
+/**
+ *
+ * @author alumno
+ */
+public interface Volador {
+    
+    public abstract String obtenerMensajeVolador();
+    public abstract void aumentarAlturaVuelo();
+    public abstract void bajarAlturaVuelo();
+    public abstract void mostrarAlturaVuelo();
+    
+}
