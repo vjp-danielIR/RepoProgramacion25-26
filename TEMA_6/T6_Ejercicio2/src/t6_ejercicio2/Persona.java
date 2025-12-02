f7223b5328cd6a205eeb1954ba8fc919bec01086
vjp-danielIR <diglesiasr04@informatica.iesvalledeljerteplasencia.es>
2025-12-02 18:05:22 +0100

    Ejercicio 2 terminado por revision falla polimorfismo

---
+++ TEMA_6/T6_Ejercicio2/src/t6_ejercicio2/Persona.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package t6_ejercicio2;
+
+/**
+ *
+ * @author alumno
+ * atributo nombre y el método abstracto saludar
+ */
+public class Persona {
+    //atributo
+    String nombre;
+    //constructor
+    public Persona(){
+    this.nombre="";
+    }
+    //parametrizado
+    public Persona(String nombre){
+    this.nombre=nombre;
+    }
+    
+    //getter y seter
+    public String getNombre(){
+    return this.nombre;
+    }
+    public void setNombre(String nombre){
+    this.nombre=nombre;
+    }
+    
+    //metodo saludar
+    public void Saludar(){
+        System.out.println("hola mi nombre es" + this.nombre);
+    }
+            
+}
