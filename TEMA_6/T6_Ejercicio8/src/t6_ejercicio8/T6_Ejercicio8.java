3c1fe0885f65bdf4b89b61dbfb657a32895d8031
vjp-danielIR <diglesiasr04@informatica.iesvalledeljerteplasencia.es>
2025-12-05 20:09:07 +0100

    EJERCICIO 8 TERMINADO

--- TEMA_6/T6_Ejercicio8/src/t6_ejercicio8/T6_Ejercicio8.java
+++ TEMA_6/T6_Ejercicio8/src/t6_ejercicio8/T6_Ejercicio8.java
@@ -8,54 +8,41 @@ package t6_ejercicio8;
  *
  * @author alumno
  */
-public class T6_Ejercicio8 {
 
-    /**
+public class T6_Ejercicio8 {
+    
+     /**
      * @param args the command line arguments
      * 
-     * 
-     * 
-     * procesarVehiculo
-(Vehiculo vehiculo). Este método no devuelve nada y realiza lo siguiente:
-• Muestra el movimiento del vehículo.
-• Si el vehículo es un coche, enciende el aire y muestra el movimiento del coche.
-• Si el vehículo es una moto, hace un caballito y muestra el movimiento de la moto.
-• Posteriormente, instancia un Vehiculo de cada tipo (Coche y Moto) e invoca al método
-procesarVehiculo pasándole ambos objetos.
-* 
-* 
-* 
      */
-    public static void main(String[] args) {
+    
+     public static void main(String[] args) {
          
-        Vehiculo coche = new Coche(80, false);
-        Vehiculo moto = new Moto(60, false );
+         Vehiculo coche = new Coche(80, false);
+        Vehiculo moto = new Moto(60, false);
 
-        System.out.println("Procesando coche ");
+        System.out.println("Procesando coche");
         procesarVehiculo(coche);
 
         System.out.println("Procesando moto");
         procesarVehiculo(moto);
-        
-    /**
-     *
-     * @param vehiculo
-     */
-    public void procesarVehiculo(Vehiculo vehiculo, Vehiculo Moto, Vehiculo Coche) {
-        // Mostrar movimiento inicial pomorfismo 
-        vehiculo.mostrarMovimiento();
-        
+    }
 
-        // Acciones específicas
-        if (Coche == vehiculo) {
+    public static void procesarVehiculo(Vehiculo vehiculo) {
+
+        // Movimiento inicial (polimorfismo)
+        vehiculo.mostrarMovimiento();
+
+        // Comportamiento dependiendo del tipo de objeto
+        if (vehiculo instanceof Coche) {
             Coche coche = (Coche) vehiculo;
             coche.encenderAire();
             coche.mostrarMovimiento();
 
-        } else if (vehiculo == Moto) {
+        } else if (vehiculo instanceof Moto) {
             Moto moto = (Moto) vehiculo;
             moto.hacerCaballito();
             moto.mostrarMovimiento();
         }
-    
-}}
+    }
+}
