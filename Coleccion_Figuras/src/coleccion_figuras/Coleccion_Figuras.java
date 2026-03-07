53b8560cc2133b4f5c9354bdad229f2823c873d7
vjp-danielIR <diglesiasr04@informatica.iesvalledeljerteplasencia.es>
2026-03-07 18:33:52 +0100

    arreglo de proyecto(estudio)

--- Coleccion_Figuras/src/coleccion_figuras/Coleccion_Figuras.java
+++ Coleccion_Figuras/src/coleccion_figuras/Coleccion_Figuras.java
@@ -1,136 +1,132 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package coleccion_figuras;
 
-import java.util.*;  //para perdir al usuario y el arrayList
+import java.util.*;
 
 /**
- *
  * @author Daniel Iglesias Ramos
  */
 public class Coleccion_Figuras {
 
-    /**
-     * @param args the command line arguments
-     */
+    // Muestra el menu y gestiona las opciones del usuario
     public static void main(String[] args) {
 
         Scanner entrada = new Scanner(System.in);
-        // Matriz
-        String[][] figura = new String[3][3];
+        Figuras[][] figura = new Figuras[3][3];
         int opcion = 0;
 
         do {
-            System.out.println("==================");
-            System.out.println("-------Menu-------");
-            System.out.println("____________________");
+            System.out.println("Menu");
             System.out.println("1. Rellenar la vitrina de figuras");
-            System.out.println("___________________________________");
             System.out.println("2. Mostrar todas las figuras de una coleccion");
-            System.out.println("______________________________________________");
             System.out.println("3. Mostrar la figura mas valiosa");
-            System.out.println("__________________________________");
             System.out.println("4. Salir del programa");
-            System.out.println("________________________");
             System.out.println("Que opcion quieres?");
 
             try {
                 opcion = entrada.nextInt();
+                entrada.nextLine();
                 switch (opcion) {
-                    case 1 ->
-                        rellenarMatriz(figura);
-
-                    case 2 ->
-                        mostrarColeccion(figura);
-                    case 3 ->
-                        mostrarMasValiosa(figura);
-                    case 4 ->
-                        System.out.println("Saliendo...");
-                    default ->
-                        System.out.println("Opcion no valida");
+                    case 1 -> rellenarMatriz(figura, entrada);
+                    case 2 -> mostrarColeccion(figura, entrada);
+                    case 3 -> mostrarMasValiosa(figura);
+                    case 4 -> System.out.println("Saliendo...");
+                    default -> System.out.println("Opcion no valida");
                 }
             } catch (InputMismatchException e) {
                 System.out.println("Valor no valido");
-                entrada.nextLine(); // limpiar la anterior entrada
+                entrada.nextLine();
                 opcion = 0;
             }
         } while (opcion != 4);
     }
 
-    // rellenar el vector con nombre y valor de cada figura
-    public static void rellenarMatriz(String[][] figuras) {
+    // Rellena la matriz 3x3 creando un objeto Figuras en cada posicion
+    public static void rellenarMatriz(Figuras[][] figuras, Scanner entrada) {
         for (int i = 0; i < figuras.length; i++) {
-            System.out.println("--- Figura " + (i + 1) + " ---");
-            figuras[i][0] = pedirNombre();
-            figuras[i][1] = String.valueOf(pedirValor());  //cambiar el tipo de double a string
-            figuras[i][2] = pedirColeccion();
-          
+            for (int j = 0; j < figuras[i].length; j++) {
+                System.out.println("Figura [" + (i + 1) + "][" + (j + 1) + "]");
+                figuras[i][j] = new Figuras(pedirNombre(entrada), pedirValor(entrada), pedirColeccion(entrada));
+            }
         }
     }
 
-    // pedir el nombre de la figura
-    public static String pedirNombre() {
-        Scanner figura = new Scanner(System.in);
+    // Pide y devuelve el nombre de la figura
+    public static String pedirNombre(Scanner entrada) {
         System.out.println("Como se llama la figura?");
-        return figura.nextLine();
+        return entrada.nextLine();
     }
 
-    // pedir el valor de la figura
-    public static double pedirValor() {
-        Scanner precio = new Scanner(System.in);
-        System.out.println("Cual es su valor?");
-
-        return precio.nextDouble();
-
+    // Pide y devuelve el valor de la figura, valida que no sea negativo
+    public static double pedirValor(Scanner entrada) {
+        double valor = 0;
+        boolean valido = false;
+        while (!valido) {
+            System.out.println("Cual es su valor?");
+            try {
+                valor = Double.parseDouble(entrada.nextLine());
+                if (valor < 0) {
+                    System.out.println("El valor no puede ser negativo.");
+                } else {
+                    valido = true;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Introduce un numero valido.");
+            }
+        }
+        return valor;
     }
-    // pedir la coleccion de donde es la figura
 
-    public static String pedirColeccion() {
-        Scanner figura = new Scanner(System.in);
+    // Pide y devuelve la coleccion de la figura
+    public static String pedirColeccion(Scanner entrada) {
         System.out.println("De que coleccion es la figura?");
-        return figura.nextLine();
+        return entrada.nextLine();
     }
 
-    // mostrar todas las figura de una coleccion determinada 
-    public static void mostrarColeccion(String[][] figuras) {
-        Scanner entrada = new Scanner(System.in);
-
+    // Muestra todas las figuras que pertenecen a una coleccion concreta
+    public static void mostrarColeccion(Figuras[][] figuras, Scanner entrada) {
         System.out.println("De que coleccion quieres ver las figuras?");
         String coleccion = entrada.nextLine();
         boolean encontrada = false;
 
         for (int i = 0; i < figuras.length; i++) {
-            //si la figura x posicion no es nul y ignorando mayusculas es igual imprimira nombre valor coleccion y la posicion en la uqe la encontro 
-            if (figuras[i][0] != null && figuras[i][2].equalsIgnoreCase(coleccion)) {
-                System.out.println("Nombre: " + figuras[i][0] + "  Valor: " + figuras[i][1] + " Coleccion: " + figuras[i][2] + " Posicion en la vitrina: " + (i + 1));
-                encontrada = true;
-            }
-        }
-        System.out.println("No se encontro otra igual");
-    }
-
-    // mostrar la figura mas valiosa
-    public static void mostrarMasValiosa(String[][] figuras) {
-        String figuraMasValiosa = "";
-        double valorMax = 0;
-
-        for (int i = 0; i < figuras.length; i++) {
-            //si figura posicion x no es null se convertira en double y si es mayor que 0 el valor max cambiara al nuevo valor y justito despues se guardara la posicion 
-            //de la figura mas valiosa por entonces
-            if (figuras[i][0] != null) {
-                double valor = Double.parseDouble(figuras[i][1]); //que conste que me lo recomendo el ide yo puse el valueOf
-                if (valor > valorMax) {
-                    valorMax = valor;
-                    figuraMasValiosa = figuras[i][0];
+            for (int j = 0; j < figuras[i].length; j++) {
+                if (figuras[i][j] != null && figuras[i][j].getColeccion().equalsIgnoreCase(coleccion)) {
+                    System.out.println("Nombre: " + figuras[i][j].getNombrePerso()
+                            + "  Valor: " + figuras[i][j].getValor()
+                            + "  Coleccion: " + figuras[i][j].getColeccion()
+                            + "  Posicion: [" + (i + 1) + "][" + (j + 1) + "]");
+                    encontrada = true;
                 }
             }
-            System.out.println("Nombre: " + figuras[i][0] + "  Valor: " + figuras[i][1] + " Coleccion: " + figuras[i][2] + " Posicion en la vitrina: " + (i + 1));
-
         }
-        
-            System.out.println("No hay figuras en la vitrina");
+        if (!encontrada) {
+            System.out.println("No se encontro ninguna figura de esa coleccion.");
         }
     }
 
+    // Busca y muestra la figura con el valor mas alto de la vitrina
+    public static void mostrarMasValiosa(Figuras[][] figuras) {
+        Figuras figuraMasValiosa = null;
+        int[] posicion = new int[2];
+
+        for (int i = 0; i < figuras.length; i++) {
+            for (int j = 0; j < figuras[i].length; j++) {
+                if (figuras[i][j] != null && (figuraMasValiosa == null || figuras[i][j].getValor() > figuraMasValiosa.getValor())) {
+                    figuraMasValiosa = figuras[i][j];
+                    posicion[0] = i;
+                    posicion[1] = j;
+                }
+            }
+        }
+
+        if (figuraMasValiosa == null) {
+            System.out.println("No hay figuras en la vitrina.");
+        } else {
+            System.out.println("La figura mas valiosa es:");
+            System.out.println("Nombre: " + figuraMasValiosa.getNombrePerso()
+                    + "  Valor: " + figuraMasValiosa.getValor()
+                    + "  Coleccion: " + figuraMasValiosa.getColeccion()
+                    + "  Posicion: [" + (posicion[0] + 1) + "][" + (posicion[1] + 1) + "]");
+        }
+    }
+}
