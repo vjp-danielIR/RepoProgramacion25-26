f8e160f1febd29421e41c246bb2eb3a0a46dc4ea
vjp-danielIR <diglesiasr04@informatica.iesvalledeljerteplasencia.es>
2026-04-30 19:33:27 +0200

    arreglo

--- TEMA_09/TEMA9_Ejercicio3/src/tema9_ejercicio3/TEMA9_Ejercicio3.java
+++ TEMA_09/TEMA9_Ejercicio3/src/tema9_ejercicio3/TEMA9_Ejercicio3.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package tema9_ejercicio3;
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +15,15 @@ public class TEMA9_Ejercicio3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Introduce una cadena de caracteres: ");
+        String cadena = entrada.nextLine();
+
+        char[] caracteres = cadena.toCharArray();
+
+        for (char c : caracteres) {
+            System.out.println(c);
+        }
     }
-    
 }
